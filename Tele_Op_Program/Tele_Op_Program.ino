74218beab4ab3b45fe240eb6a40345445ed33f50
srisaipog <sridhar.sairam21@ycdsbk12.ca>
2019-10-18 12:46:11 -0400

    Started tele-op file

---
+++ Tele_Op_Program/Tele_Op_Program.ino
@@ -0,0 +1,27 @@
+
+// Tele_Op_Program
+// Using PS4 Controller
+
+// Sridhar Sairam
+
+// October 2019
+
+#include <PRIZM.h>
+#include <TELEOP.h>
+// #include <PULSE.h>
+
+PRIZM prizm;
+TELEOP ps4;
+PULSE pulse;
+
+
+void setup()
+{
+  
+}
+
+
+void loop()
+{
+  
+}
