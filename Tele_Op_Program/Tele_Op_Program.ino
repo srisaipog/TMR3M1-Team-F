09f6a78f8ab585f27a14349a4f789e73afca60e9
srisaipog <sridhar.sairam21@ycdsbk12.ca>
2019-10-18 13:07:21 -0400

    Worked on Tele op program

--- Tele_Op_Program/Tele_Op_Program.ino
+++ Tele_Op_Program/Tele_Op_Program.ino
@@ -8,20 +8,50 @@
 
 #include <PRIZM.h>
 #include <TELEOP.h>
-// #include <PULSE.h>
+#include <PULSE.h>
 
 PRIZM prizm;
-TELEOP ps4;
-PULSE pulse;
+PS4 ps4;
+// PULSE pulse;
+
+// Analog sticks
+int left_y, left_x, right_y, right_x;
+
+// Buttons
+bool left, right, down, up, x, y, a, b;
 
 
 void setup()
 {
-  
+  ps4.getPS4();
+  Serial.begin(9600);
 }
 
 
 void loop()
 {
+  getMotors();
+  
+  // left_y = map(left_y, 0, 255, -00, 100);
+  // left_x = map(left_x, 0, 255, -100, 100);
+
+  Serial.write(left_x);
+  Serial.write(left_y);
+  //Serial.write("");
+  delay(500);
+  
+
+  // prizm.setMotorSpeeds(left_);
+  
+}
+
+void getMotors()
+{
+  ps4.getPS4();
+  left_y = ps4.Stick(LY);
+  left_x = ps4.Stick(LX);
+
+  right_y = ps4.Stick(RY);
+  right_x = ps4.Stick(RX);
   
 }
